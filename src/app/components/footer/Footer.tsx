af82626b849c623b3f3505a195c619ffa6d4613f
Alif-khan-2002 <alifr2648@gmail.com>
2025-05-15 22:33:50 +0600

    footer title

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      <header>
+        <title>Thsi is Footer</title>
+      </header>
       <h1>Hello I am Alif Khan</h1>
 
     </div>
