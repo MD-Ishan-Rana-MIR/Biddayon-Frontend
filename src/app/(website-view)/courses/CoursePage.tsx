260eb2b4a7d3c056aff8c9991b1c73656a21bae0
ishanrana363 <ishanrana363@gmail.com>
2025-05-23 22:11:11 +0600

    deploy completed

--- src/app/(website-view)/courses/CoursePage.tsx
+++ src/app/(website-view)/courses/CoursePage.tsx
@@ -338,6 +338,8 @@ const CoursesPage: React.FC = () => {
                                 </Select>
                             </div>
 
+                            {/* loem23 */}
+
                             <TabsContent value="all" className="mt-0">
                                 <Suspense fallback={<Loading />}>
                                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
